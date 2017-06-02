aad2cc6c829f5fc0b2fa153c2de03aecf0ce9af9
raphael <>
2017-06-02 16:39:48 +0900

    Initial commit

---
+++ app/src/main/java/kr/iuu/samplereceiver/SmsActivity.java
@@ -0,0 +1,56 @@
+package kr.iuu.samplereceiver;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SmsActivity extends AppCompatActivity {
+
+    TextView textView;
+    TextView textView2;
+    TextView textView3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Intent passedIntent = getIntent();
+        processIntent(passedIntent);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        processIntent(intent);
+
+        super.onNewIntent(intent);
+    }
+
+    private void processIntent(Intent intent) {
+        if (intent != null) {
+            String sender = intent.getStringExtra("sender");
+            String contents = intent.getStringExtra("contents");
+            String receivedDate = intent.getStringExtra("receivedDate");
+
+            textView.setText(sender);
+            textView2.setText(contents);
+            textView3.setText(receivedDate);
+        }
+    }
+}
